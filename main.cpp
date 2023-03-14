94bd6aa6cb17bba5940b153599f5625495c88faa
LilithsDemon <lucy.gardener2004@gmail.com>
2023-03-14 01:10:00 +0000

    Starting to add accounts with passwords

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <stdlib.h>
+
+
+//A salt is used to prepend to a password this allows for better security
+//For a salt to work properly it has to be different each time the program is used by someone
+//It also has to be random and long this will get prepended to the hashed password
+
+
+std::string generateSalt()
+{
+    std::srand(time(0)); //seeding random generator for c++ random numbers
+    const int salt_size = 10; //How long we want the salt
+    //A longer salt should be used so that is is not easy to create a solution to hashes
+    char result[salt_size]; //Array to hold each char
+    std::string salt;
+    for (int i = 0; i < salt_size; i++)
+    {
+        int random_letter_val = std::rand()%25; //Random number for each letter of the alphabet
+        char random_letter = static_cast<char> (random_letter_val + 'a'); // generating random char for the salt
+        salt = salt + random_letter; //adds random char to the salt
+    }
+    return salt;
+}
+
+unsigned long hashPassword(std::string password, std::string salt)
+{
+    std::hash <std::string> hash; //Initialises Hash
+    std::string complete_password = password + salt; //append salt to given password
+    unsigned long hashed_password = hash(complete_password); //has complete password
+    return hashed_password;
+}
+
+bool passwordCheck(std::string given_password, unsigned long saved_password, std::string salt)
+{
+    if(hashPassword(given_password, salt) == saved_password) //If hashing given password with same salt makes saved password then it is correct password
+    {
+        return true;
+    }
+    return false;
+}
+
+void createNewAccount(std::string username, unsigned long encrypted_password, std::string salt, int access)
+{
+    
+}
+
+void startMenu()
+{
+    std::cout << "___Welcome to NHS Console___" << "\n\n";
+    std::cout << "What would you like to do" << "\n";
+    while(true)
+    {
+        std::cout << "1. Login" << "\n";
+        std::cout << "2. Register for an account" << "\n";
+        std::cout << "3. Exit" << "\n";
+        int option;
+        std::cin >> option;
+        switch(option)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                exit(0);
+                break;
+            default:
+                std::cout << "\n";
+        }
+    }
+
+}
+
+int main()
+{
+    startMenu();
+    
+    return 0;
+}
